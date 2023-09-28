c6db1beed27ac0d2d5c8066546567036c58ece56
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-28 20:11:48 +0530

    accountValidation

--- validation/accountValidation.js
+++ validation/accountValidation.js
@@ -1,9 +1,9 @@
 const validateOnCreate = async(req,res,next) => {
 
-    if(!req.body?.companyName){
+    if(!req.body?.accountName){
 
        return res.status(400).json({
-            message: "companyName Required",
+            message: "accountName Required",
             varient: "error"
         })
     }
