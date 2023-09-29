6fe12a4e60aa4b3f19507ada9546a5dd19b3537b
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-29 21:05:57 +0530

    naam run

--- routes/api/v1/auth/company/addCompany.js
+++ routes/api/v1/auth/company/addCompany.js
@@ -106,7 +106,7 @@ router.delete("/deleteOne/:id",async(req,res) => {
 async function getCompanyObj(req,type){
     let newCompany = {}
     if(req.body.name) {
-        newCompany.Company = req.body.Company
+        newCompany.name = req.body.Company
     }
     if(req.body.label) {
         newCompany.label = req.body.label
