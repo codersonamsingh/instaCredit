8fc7e10c7ce643289324af210db824796e856095
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-09-29 21:11:45 +0530

    addrss

--- routes/api/v1/auth/company/addCompany.js
+++ routes/api/v1/auth/company/addCompany.js
@@ -109,7 +109,7 @@ async function getCompanyObj(req,type){
         newCompany.name = req.body.name
     }
     if(req.body.address) {
-        newCompany.label = req.body.label
+        newCompany.address = req.body.label
     }
     if(req.body.link) {
         newCompany.link = req.body.link
