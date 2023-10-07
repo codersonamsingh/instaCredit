850f3de0a8bd78be78d56bd78736733086c53fe8
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-07 19:04:58 +0530

    processingFee

--- routes/api/v1/loan/addLoan.js
+++ routes/api/v1/loan/addLoan.js
@@ -178,7 +178,7 @@ async function getaccountObj(req,type){
         newaccount.interestRate = req.body.interestRate
     }
     if(req.body.processingFee) {
-        newaccount.processingFee = req.body. gender
+        newaccount.processingFee = req.body.processingFee
     }
     if(req.body. gender) {
         newaccount. gender = req.body. gender
