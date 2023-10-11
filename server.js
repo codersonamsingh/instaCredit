48c96e39550e8634580517b70c131a3d15da61cc
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-11 20:57:48 +0530

    addCompany run

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./strategies/jsonwtStrategy")(passport)
 //call all route
 app.use("/api/v1/accounts/account/addAccount",addAccount);
 app.use("/api/v1/accounts/account/getAccount",getAccount);
-app.use("/api/v1/accounts/account/getAccount",getAccount);
+app.use("/api/v1/accounts/company/addCompany",addCompany);
 
 
 
