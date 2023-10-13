0cff404403d932972bb9f65bc78bc37746174df2
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-13 17:14:50 +0530

    mobileNumberVerified

--- routes/api/v1/loan/addLoan.js
+++ routes/api/v1/loan/addLoan.js
@@ -201,7 +201,7 @@ async function getaccountObj(req,type){
     if(req.body.mobileNumber) {
         newaccount. guarantor = req.body. guarantor
     }
-    if(req.body. guarantor) {
+    if(req.body.mobileNumberVerified) {
         newaccount. guarantor = req.body. guarantor
     }
     if(req.body. guarantor) {
