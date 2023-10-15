c1fdd370d1e9d2185d08c9ea2324da09631916be
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-15 20:45:13 +0530

    user run

--- routes/api/v1/accounts/account/loan/getLoan.js
+++ routes/api/v1/accounts/account/loan/getLoan.js
@@ -71,7 +71,7 @@ router.get("/search/:searchQuery", async(req,res) => {
         const getLoan = await Loan.find({
             $or:[
                 {company: new RegExp(searchQuery, "i")},
-                {: new RegExp(searchQuery, "i")},
+                {user: new RegExp(searchQuery, "i")},
                 
             ]
         })
