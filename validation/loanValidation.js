c0ad2d03088461e32d810d92348288f0da74c305
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-16 20:34:20 +0530

    ramova nam

--- validation/loanValidation.js
+++ validation/loanValidation.js
@@ -3,7 +3,7 @@ const validateOnCreate = async(req,res,next) => {
     if(!req.body?.company){
 
        return res.status(400).json({
-            message: "name Required",
+            message: "nam Required",
             varient: "error"
         })
     }
