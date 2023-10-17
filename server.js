d574775dc34c0c93d184b67c901d6c7b73f7b89d
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-17 20:44:41 +0530

    loan

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/v1/accounts/account/addAccount",addAccount);
 app.use("/api/v1/accounts/account/getAccount",getAccount);
 app.use("/api/v1/accounts/company/addCompany",addCompany);
 app.use("/api/v1/accounts/company/getCompany",getCompany);
-app.use("/api/v1/accounts/company/addLoan",addLoan);
+app.use("/api/v1/accounts/loan/addLoan",addLoan);
 app.use("/api/v1/accounts/company/getCompany",getCompany);
 
 
