d6c9d142459d83a2bbea27ff43044d1c0a457916
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-18 21:19:58 +0530

    module account run

--- routes/api/v1/accounts/account/addAccount.js
+++ routes/api/v1/accounts/account/addAccount.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const account = require("../../../../../");
+const account = require("../../../../../models/Account");
 const router = express.Router();
 const {validateOnCreate, validateOnUpdate} = require("../../../../../validation/accountValidation")
 
