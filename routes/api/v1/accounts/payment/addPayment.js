4f8b5a0068c5e3820943fd64d40994c597635805
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-21 23:06:18 +0530

    add ruinn

--- routes/api/v1/accounts/payment/addPayment.js
+++ routes/api/v1/accounts/payment/addPayment.js
@@ -108,7 +108,7 @@ async function getPaymentObj(req,type){
         newPayment.amount = req.body.amount
     }
     if(req.body.isCredit) {
-        newPayment.link = req.body.link
+        newPayment. = req.body.link
     }
      if(req.body.notes) {
         newPayment.notes = req.body.notes
