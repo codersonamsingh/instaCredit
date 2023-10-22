ef73046f42688ef83bee2e052afe59ed9f7b99b5
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-22 23:55:04 +0530

    validation runnn

--- validation/accountValidation.js
+++ validation/accountValidation.js
@@ -3,7 +3,7 @@ const validateOnCreate = async(req,res,next) => {
     if(!req.body?.label){
 
        return res.status(400).json({
-            message: "Company Required",
+            message: "label Required",
             varient: "error"
         })
     }
