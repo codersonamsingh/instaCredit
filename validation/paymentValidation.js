c54bce61dd7c5c2dcb4c4af98f902f5744cad9e4
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-10-29 22:05:41 +0530

    amu

--- validation/paymentValidation.js
+++ validation/paymentValidation.js
@@ -3,7 +3,7 @@ const validateOnCreate = async(req,res,next) => {
     if(!req.body?.amount){
 
        return res.status(400).json({
-            message: "name Required",
+            message: " Required",
             varient: "error"
         })
     }
